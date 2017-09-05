f50f1a97f4a108d46983f25d6920e06b4feb5735
belth0219 <flyer0339@gmail.com>
2017-09-05 17:00:25 +0800

    1

--- wahes/mainsite/views.py
+++ wahes/mainsite/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
 
 # Create your views here.
